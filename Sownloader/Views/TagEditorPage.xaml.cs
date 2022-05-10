be484b71cd8465d7c40d6ef35e2ee21ca77c3bc1
Marvin Klein <support@marvinkleinmusic.de>
2022-05-10 18:54:50 +0200

    Added TagEditorPage

---
+++ Sownloader/Views/TagEditorPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Sownloader.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sownloader.Views
+{
+    /// <summary>
+    /// Interaktionslogik für TagEditorPage.xaml
+    /// </summary>
+    public partial class TagEditorPage : Page
+    {
+        public TagEditorPage(TagEditorViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
