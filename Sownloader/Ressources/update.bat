99e1fa004c16185d3a93cc2469acc0a15e4f5528
Marvin Klein <support@marvinkleinmusic.de>
2022-08-28 16:58:35 +0200

    Update update.bat for V9

--- Sownloader/Ressources/update.bat
+++ Sownloader/Ressources/update.bat
@@ -1,11 +1,69 @@
+:: Version 1.2.0
 @echo off
 if %1.==. goto: FEHLER
 if %2.==. goto: FEHLER
 if not exist %2 goto: FEHLER 
 
-for %%i in (*.*) do if not "%%i"=="Sownloader.zip" if not "%%i"=="sownloader.log" if not "%%i"=="update.bat" del /q "%%i"
+taskkill /f /im Sownloader.exe
+timeout /t 2
 
-setlocal
+:: Delete old Sownloader files, do not delete with loop because user could have other files in same directory
+del /s /q /f %1runtimes\*.*
+del /s /q /f %1DarkControls.dll
+del /s /q /f %1DarkControls.pdb
+del /s /q /f %1ffmpeg.exe
+del /s /q /f %1Microsoft.Extensions.Configuration.Abstractions.dll
+del /s /q /f %1Microsoft.Extensions.Configuration.Binder.dll
+del /s /q /f %1Microsoft.Extensions.Configuration.CommandLine.dll
+del /s /q /f %1Microsoft.Extensions.Configuration.dll
+del /s /q /f %1Microsoft.Extensions.Configuration.EnvironmentVariables.dll
+del /s /q /f %1Microsoft.Extensions.Configuration.FileExtensions.dll
+del /s /q /f %1Microsoft.Extensions.Configuration.Json.dll
+del /s /q /f %1Microsoft.Extensions.Configuration.UserSecrets.dll
+del /s /q /f %1Microsoft.Extensions.DependencyInjection.Abstractions.dll
+del /s /q /f %1Microsoft.Extensions.DependencyInjection.dll
+del /s /q /f %1Microsoft.Extensions.DependencyModel.dll
+del /s /q /f %1Microsoft.Extensions.FileProviders.Abstractions.dll
+del /s /q /f %1Microsoft.Extensions.FileProviders.Physical.dll
+del /s /q /f %1Microsoft.Extensions.FileSystemGlobbing.dll
+del /s /q /f %1Microsoft.Extensions.Hosting.Abstractions.dll
+del /s /q /f %1Microsoft.Extensions.Logging.Configuration.dll
+del /s /q /f %1Microsoft.Extensions.Logging.Console.dll
+del /s /q /f %1Microsoft.Extensions.Logging.Debug.dll
+del /s /q /f %1Microsoft.Extensions.Logging.dll
+del /s /q /f %1Microsoft.Extensions.Logging.EventLog.dll
+del /s /q /f %1Microsoft.Extensions.Logging.EventSource.dll
+del /s /q /f %1Microsoft.Extensions.Options.ConfigurationExtensions.dll
+del /s /q /f %1Microsoft.Extensions.Options.dll
+del /s /q /f %1Microsoft.Extensions.Primitives.dll
+del /s /q /f %1Microsoft.Web.WebView2.Core.dll
+del /s /q /f %1Microsoft.Web.WebView2.WinForms.dll
+del /s /q /f %1Microsoft.Web.WebView2.Wpf.dll
+del /s /q /f %1Microsoft.WindowsAPICodePack.dll
+del /s /q /f %1Microsoft.WindowsAPICodePack.ExtendedLinguisticServices.dll
+del /s /q /f %1Microsoft.WindowsAPICodePack.Sensors.dll
+del /s /q /f %1Microsoft.WindowsAPICodePack.Shell.dll
+del /s /q /f %1Microsoft.WindowsAPICodePack.ShellExtensions.dll
+del /s /q /f %1Serilog.dll
+del /s /q /f %1Serilog.Extensions.Hosting.dll
+del /s /q /f %1Serilog.Extensions.Logging.dll
+del /s /q /f %1Serilog.Settings.Configuration.dll
+del /s /q /f %1Serilog.Sinks.Console.dll
+del /s /q /f %1Serilog.Sinks.File.dll
+del /s /q /f %1Sownloader.Core.dll
+del /s /q /f %1Sownloader.Core.pdb
+del /s /q /f %1Sownloader.deps.json
+del /s /q /f %1Sownloader.dll
+del /s /q /f %1Sownloader.exe
+del /s /q /f %1Sownloader.pdb
+del /s /q /f %1Sownloader.runtimeconfig.json
+del /s /q /f %1System.Diagnostics.EventLog.Messages.dll
+del /s /q /f %1taglib-sharp.dll
+del /s /q /f %1WebView2Loader.dll
+
+::for %%i in (*.*) do if not "%%i"=="Sownloader.zip" if not "%%i"=="sownloader.log" if not "%%i"=="update.bat" if not "%%i"=="config.ini" del /q "%%i"
+
+setlocal 
 cd /d %~dp0
 Call :UnZipFile %1 %2
 exit /b
@@ -30,6 +88,5 @@ start %1% Sownloader.exe -createbatch
 exit
 goto:eof
 :FEHLER
-echo invalid request
 pause
 
