ecbc57ff3016a702d2a7f73208fb02129db3831b
Marvin Klein <support@marvinkleinmusic.de>
2022-09-14 23:55:30 +0200

    Add code to fetch current version of Sownloader

---
+++ Sownloader.Tests/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Sownloader.Core;
+using System.Reflection;
+
+Version version = await UpdateSearch.SearchForUpdateAsync(Assembly.GetExecutingAssembly().GetName().Version);
+
+
+Console.WriteLine(version);
