8996239ba926cf5bbe46f80fea66a0b8aa5cead4
Marvin Klein <support@marvinkleinmusic.de>
2022-09-20 18:51:29 +0200

    Save response for debugging

--- Sownloader.Core/UpdateSearch.cs
+++ Sownloader.Core/UpdateSearch.cs
@@ -51,7 +51,7 @@ namespace Sownloader.Core
         {
             // Update download stats for latest version
             // Website does not return anything
-            await _client.GetAsync($"https://sownloader.com/functions/updater.php?action=updateDatabase&version={LatestVersion}&code=ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff");
+            var response = await _client.GetAsync($"https://sownloader.com/functions/updater.php?action=updateDatabase&version={LatestVersion}&code=ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff");
 
             // Download the ZIP file.
             Downloader downloader = new Downloader();
